d7deb801f0710e9c6aa39d2beaf7fc65d3874e55
Shacklebottom <korzinekt@gmail.com>
2026-04-13 13:11:54 -0500

    added getters and setters to Card.java

--- src/main/java/com/ouroborosgames/dominioncompanionredux/model/Card.java
+++ src/main/java/com/ouroborosgames/dominioncompanionredux/model/Card.java
@@ -6,4 +6,20 @@ public class Card {
     private CardTypes[] types;
     private String[] expansions;
     private Boolean provinceFlag;
+
+    //getters
+    public String getName() { return name; }
+    public int getCost() { return cost; }
+    public CardTypes[] getTypes() { return types; }
+    public String[] getExpansions() { return expansions; }
+    public Boolean getProvinceFlag() { return provinceFlag; }
+
+    //setters
+    public void setName(String name) { this.name = name; }
+    public void setCost(int cost) { this.cost = cost; }
+    public void setTypes(CardTypes[] types) { this.types = types; }
+    public void setExpansions(String[] expansions) { this.expansions = expansions; }
+    public void setProvinceFlag(Boolean provinceFlag) { this.provinceFlag = provinceFlag; }
+    
 }
+
