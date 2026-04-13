99749cd5581acc55598813d561880de52c80c968
Shacklebottom <korzinekt@gmail.com>
2026-04-13 11:21:08 -0500

    initial commit two, the committening

---
+++ src/main/java/com/ouroborosgames/dominioncompanionredux/DominionCompanionReduxApplication.java
@@ -0,0 +1,13 @@
+package com.ouroborosgames.dominioncompanionredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominionCompanionReduxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DominionCompanionReduxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ouroborosgames/dominioncompanionredux/DominionCompanionReduxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouroborosgames.dominioncompanionredux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominionCompanionReduxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
