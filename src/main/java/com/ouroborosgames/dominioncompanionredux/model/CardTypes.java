197d07fba7b0b4d0f2718165ef8609707e89573f
Shacklebottom <korzinekt@gmail.com>
2026-04-13 13:01:01 -0500

    added the enum CardTypes.java

---
+++ src/main/java/com/ouroborosgames/dominioncompanionredux/model/CardTypes.java
@@ -0,0 +1,21 @@
+package com.ouroborosgames.dominioncompanionredux.model;
+
+public enum CardTypes {
+    Reaction,
+    Economy,
+    Trash,
+    Draw,
+    Cantrip,
+    Village,
+    Gain,
+    Attack,
+    Buy,
+    MajorVictory,
+    UtilityVictory,
+    Ally,
+    Event,
+    Landmark,
+    Way,
+    Project,
+    Trait;
+}
