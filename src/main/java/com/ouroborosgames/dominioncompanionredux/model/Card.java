f95ae4b6792aa7a3e2b73b9b56b01e74815e68d5
Shacklebottom <korzinekt@gmail.com>
2026-04-13 11:34:07 -0500

    added 'model' package and Card.java

---
+++ src/main/java/com/ouroborosgames/dominioncompanionredux/model/Card.java
@@ -0,0 +1,9 @@
+package com.ouroborosgames.dominioncompanionredux.model;
+
+public class Card {
+    private String name;
+    private int cost;
+    private String[] types;
+    private String[] expansions;
+    private Boolean provinceFlag;
+}
